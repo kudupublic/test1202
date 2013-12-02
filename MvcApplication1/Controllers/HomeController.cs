be5178662c7f622e74de2b5e8c84b41de46f991e
Lizzytest <kudutest@outlook.com>
2013-12-02 15:00:29 +0800

    mvc

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Lizzy0807 ASP.NET MVC application ";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your quintessential app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your quintessential contact page.";
+
+            return View();
+        }
+    }
+}
